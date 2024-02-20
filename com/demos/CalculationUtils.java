6ee9ac404498057f95deae1da627fec4b87396d4
VaidehiAnil.Zade <VaidehiAnil.Zade@cantire.com>
2024-02-20 20:15:25 +0530

    copilot trial

--- com/demos/CalculationUtils.java
+++ com/demos/CalculationUtils.java
@@ -5,4 +5,8 @@ public class CalculationUtils {
     public double calculateDiscount(double price, double discount) {
         return price * discount / 100;
     }
+
+    public double calculateTax(double price, double tax) {
+        return price * tax / 100;
+    }
 }
